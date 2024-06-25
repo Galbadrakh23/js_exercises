6b192e54f89074a3066b5f0a02131ac0c614ed1d
galbadrakh <galbadrak223@gmail.com>
2024-06-25 09:00:49 +0800

    modified

--- dom/main.js
+++ dom/main.js
@@ -1,43 +1,67 @@
-// console.log("Working");
+// // console.log("Working");
 
-// function change() {
-//   document.getElementById("box").style.backgroundColor = "Yellow";
+// // function change() {
+// //   document.getElementById("box").style.backgroundColor = "Yellow";
 
-//   console.log("Button clicked");
+// //   console.log("Button clicked");
+// // }
+
+// // function secondChange() {
+// //   document.getElementById("box").style.backgroundColor = "Black";
+// // }
+// // const btnEl = document.getElementById("btn");
+
+// // btnEl.addEventListener("click", change);
+
+// const boxEL = document.getElementsByClassName("box")[0];
+// const btnEL = document.getElementsByClassName("btn")[0];
+// console.log(boxEL);
+// console.log(btnEL);
+
+// const colors = ["green", "yellow", "grey"];
+// let i = 0;
+
+// function generateColor() {
+//   const rColor = Math.floor(Math.random() * 256);
+//   const gColor = Math.floor(Math.random() * 256);
+//   const bColor = Math.floor(Math.random() * 256);
+//   //   return "rgb(" + rColor + "," + gColor + "," + bColor + ")";
+//   return `rgb(${rColor},${gColor},${bColor})`; // Template Script
 // }
 
-// function secondChange() {
-//   document.getElementById("box").style.backgroundColor = "Black";
+// function changeBackroundColor() {
+//   console.log("changed");
+
+//   boxEL.style.backgroundColor = generateColor();
+
+//   //   i++;
+//   //   if (i === colors.length) {
+//   //     i = 0;
+//   //   }
 // }
-// const btnEl = document.getElementById("btn");
 
-// btnEl.addEventListener("click", change);
+// btnEL.addEventListener("click", changeBackroundColor);
 
-const boxEL = document.getElementsByClassName("box")[0];
+// =================HM==========================
+
+const innerBoxes = document.getElementsByClassName("inner-box");
+
 const btnEL = document.getElementsByClassName("btn")[0];
-console.log(boxEL);
-console.log(btnEL);
-
-const colors = ["green", "yellow", "grey"];
-let i = 0;
 
 function generateColor() {
   const rColor = Math.floor(Math.random() * 256);
   const gColor = Math.floor(Math.random() * 256);
   const bColor = Math.floor(Math.random() * 256);
-  //   return "rgb(" + rColor + "," + gColor + "," + bColor + ")";
-  return `rgb(${rColor},${gColor},${bColor})`; // Template Script
+  return `rgb(${rColor},${gColor},${bColor})`;
 }
 
-function changeBackroundColor() {
+// Хайрцаг тус бүрийн өнгийг солих
+
+function changeBackgroundColor() {
   console.log("changed");
-
-  boxEL.style.backgroundColor = generateColor();
-
-  //   i++;
-  //   if (i === colors.length) {
-  //     i = 0;
-  //   }
+  for (let i = 0; i < innerBoxes.length; i++) {
+    innerBoxes[i].style.backgroundColor = generateColor();
+  }
 }
 
-btnEL.addEventListener("click", changeBackroundColor);
+btnEL.addEventListener("click", changeBackgroundColor);
